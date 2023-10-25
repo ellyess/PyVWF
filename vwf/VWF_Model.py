8c3ae36d7e274e6470af353c560bb865fb86e640
Ellyess Benmoufok <benmoufok.ellyess@gmail.com>
2023-10-25 16:16:05 +0100

    fixed singlepoint and variable names
    this iteration I have managed to make sure my singlepoint input works and have cleaned up the code to try use the same variable names across files.

--- vwf/VWF_Model.py
+++ vwf/VWF_Model.py
@@ -3,8 +3,11 @@ import xarray as xr
 from pathlib import Path
 import time
 
+import vwf.prep as prep
+
 from vwf.preprocessing import (
     prep_era5,
+    prep_obs
 )
 # make a way for the country we want to work on to be read
 
@@ -23,78 +26,75 @@ class VWF():
     """
     This class allows both the training and testing of the VWF model.
     """
-    def __init__(self, country):
-
-        # variables for training and testing
-        self.mode = 'era5'
-        self.method = 'method_2'
+    def __init__(self, country, year_star, year_end, year_test):
         self.country = country
+        self.year_star = year_star
+        self.year_end = year_end
+        self.year_test = year_test
         
         # fixed inputs files for now
     
-    def load_files(self, year_star, year_end, year_test):
+    def prep(self):
         
         powerCurveFileLoc = 'data/turbine_info/Wind Turbine Power Curves.csv'
         self.powerCurveFile = pd.read_csv(powerCurveFileLoc)
         
         # files for training
-        self.era5_train = prep_era5(year_star, year_end)
-        self.obs_cf = pd.read_csv('data/wind_data/'+self.country+'/obs_cf.csv')
-        self.turb_info_train = pd.read_csv('data/wind_data/'+self.country+'/turb_info.csv')
+        self.era5_train = prep_era5(self.year_star, self.year_end)
+        self.obs_cf, self.turb_info_train = prep_obs(self.country, self.year_star, self.year_end)
+        # self.obs_cf = pd.read_csv('data/wind_data/'+self.country+'/obs_cf_train.csv')
+        # self.turb_info_train = pd.read_csv('data/wind_data/'+self.country+'/turb_info_train.csv')
         self.unc_ws_train, self.unc_cf_train = simulate_wind(self.era5_train, self.turb_info_train, self.powerCurveFile)     
            
         # files for testing
         # this file is currently saved from atlite's cutout
-        ncFile = 'data/reanalysis/era5/'+str(year_test)+'-'+str(year_test)+'_clean.nc'
+        ncFile = 'data/reanalysis/era5/'+str(self.year_test)+'-'+str(self.year_test)+'_clean.nc'
         era5_test = xr.open_dataset(ncFile)
         self.era5_test = era5_test.resample(time='1D').mean() # hourly is too slow
-           
-        self.year_star = year_star
-        self.year_end = year_end
-        self.year_test = year_test
-        
+    
         return self
         
         
     def train(self, num_clu):
 
-        my_file = Path('data/bias_correction/'+self.mode+'_'+self.method+'_bcfactors_'+str(self.year_star)+'-'+str(self.year_end)+'_'+str(num_clu)+'_clusters.csv')
+        my_file = Path('data/bias_correction/bc_factors_'+str(self.year_star)+'-'+str(self.year_end)+'_'+str(num_clu)+'.csv')
         
         if my_file.is_file():
-            print(self.method, " using ", num_clu,  "clusters is trained already.")
+            print(num_clu,  "clusters is trained already.")
             print(" ")
             
             self.clus_info = pd.read_csv(
-                'data/bias_correction/'+self.mode+'_'+self.method+'_clus_labels_'+str(self.year_star)+'-'+str(self.year_end)+'_'+str(num_clu)+'_clusters.csv',
+                'data/bias_correction/clus_labels_'+str(self.year_star)+'-'+str(self.year_end)+'_'+str(num_clu)+'.csv',
                 index_col=None
                 )
             self.bc_factors = pd.read_csv(
-                'data/bias_correction/'+self.mode+'_'+self.method+'_bcfactors_'+str(self.year_star)+'-'+str(self.year_end)+'_'+str(num_clu)+'_clusters.csv',
+                'data/bias_correction/bc_factors_'+str(self.year_star)+'-'+str(self.year_end)+'_'+str(num_clu)+'.csv',
                 index_col=None
                 )
         
         else:  
-            print("Training for ", self.method, " using ", num_clu,  " is taking place.")
+            print("Training on ", num_clu,  " clusters.")
             start_time = time.time()
             
             train_data, self.clus_info = generate_training_data(self.unc_cf_train, self.obs_cf, self.turb_info_train, num_clu)
             bcfactors = training_bias(train_data, self.era5_train, self.clus_info, self.year_star, self.year_end, self.powerCurveFile)
             self.bc_factors = format_bcfactors(bcfactors)
             
+            # SAVING FILES FOR FUTURE RE RUNS
             self.clus_info.to_csv(
-                'data/bias_correction/'+self.mode+'_'+self.method+'_clus_labels_'+str(self.year_star)+'-'+str(self.year_end)+'_'+str(num_clu)+'_clusters.csv',
+                'data/bias_correction/clus_labels_'+str(self.year_star)+'-'+str(self.year_end)+'_'+str(num_clu)+'.csv',
                 index = None
                 )
              
             self.bc_factors.to_csv(
-                'data/bias_correction/'+self.mode+'_'+self.method+'_bcfactors_'+str(self.year_star)+'-'+str(self.year_end)+'_'+str(num_clu)+'_clusters.csv',
+                'data/bias_correction/bc_factors_'+str(self.year_star)+'-'+str(self.year_end)+'_'+str(num_clu)+'.csv',
                 index = None
                 )
             
-            bcfactors.to_csv(
-                'data/bias_correction/'+self.mode+'_'+self.method+'_bcfactors_'+str(self.year_star)+'-'+str(self.year_end)+'_'+str(num_clu)+'_clusters_raw.csv',
-                index = None
-                )
+            # bcfactors.to_csv(
+            #     'data/bias_correction/bc_fact_'+str(self.year_star)+'-'+str(self.year_end)+'_'+str(num_clu)+'_raw.csv',
+            #     index = None
+            #     )
                 
             end_time = time.time()
             elapsed_time = end_time - start_time
@@ -108,41 +108,30 @@ class VWF():
     
     def test(self, time_res):
         
-        turb_info_test = pd.read_csv('data/wind_data/'+self.country+'/turb_info_test.csv')                            
-        turb_info_test = closest_cluster(self.clus_info, turb_info_test)
+        # user_input = pd.read_csv('data/wind_data/'+self.country+'/turb_info.csv')
+        user_input = pd.read_csv('data/input.csv')
+                                 
+        turb_info = closest_cluster(self.clus_info, user_input)
         
         # producing uncorrected results
-        uncorr_file = Path('data/results/raw/'+self.mode+'_'+self.method+'_'+str(self.year_test)+'_speed_uncorr.csv')
-        if uncorr_file.is_file():
-            unc_ws_test = pd.read_csv('data/results/raw/'+self.mode+'_'+self.method+'_'+str(self.year_test)+'_speed_uncorr.csv', parse_dates=['time'])
-            unc_cf_test = pd.read_csv('data/results/raw/'+self.mode+'_'+self.method+'_'+str(self.year_test)+'_cf_uncorr.csv', parse_dates=['time'])
+        unc_ws, unc_cf = simulate_wind(self.era5_test, turb_info, self.powerCurveFile)
         
-        else:
-            unc_ws_test, unc_cf_test = simulate_wind(self.era5_test, turb_info_test, self.powerCurveFile)
-            
-            unc_ws_test.to_csv('data/results/raw/'+self.mode+'_'+self.method+'_'+str(self.year_test)+'_speed_uncorr.csv', index = None)
-            unc_cf_test.to_csv('data/results/raw/'+self.mode+'_'+self.method+'_'+str(self.year_test)+'_cf_uncorr.csv', index = None)
+        unc_ws.to_csv('data/results/raw/'+str(self.year_test)+'_unc_ws.csv', index = None)
+        unc_cf.to_csv('data/results/raw/'+str(self.year_test)+'_unc_cf.csv', index = None)
             
         # producing corrected results
-        corr_file = Path('data/results/raw/'+self.mode+'_'+self.method+'_'+str(self.year_test)+'_'+time_res+'_'+str(self.num_clu)+'_clusters_speed_corr.csv')
-        if corr_file.is_file():
-            print("Results for ", self.year_test, " using ", self.num_clu, " clusters with time resolution: ", time_res, " exist already.")
-            print(" ")
-                       
-        else:
-            print("Test for ", self.year_test, " using ", self.num_clu, " clusters with time resolution: ", time_res,  " is taking place.")
-            start_time = time.time()
-            
-            corr_speed, corr_cf = simulate_wind(self.era5_test, turb_info_test, self.powerCurveFile, time_res, False, True, self.bc_factors)
-            corr_speed.to_csv('data/results/raw/'+self.mode+'_'+self.method+'_'+str(self.year_test)+'_'+time_res+'_'+str(self.num_clu)+'_clusters_speed_corr.csv', index = None)
-            corr_cf.to_csv('data/results/raw/'+self.mode+'_'+self.method+'_'+str(self.year_test)+'_'+time_res+'_'+str(self.num_clu)+'_clusters_cf_corr.csv', index = None)
-            
-            end_time = time.time()
-            elapsed_time = end_time - start_time
-            print("Results completed and saved. Elapsed time: {:.2f} seconds".format(elapsed_time))
-            print(" ")
+        print("Test for ", self.year_test, " using ", self.num_clu, " clusters with time resolution: ", time_res,  " is taking place.")
+        start_time = time.time()
         
-
+        cor_ws, cor_cf = simulate_wind(self.era5_test, turb_info, self.powerCurveFile, time_res, False, True, self.bc_factors)
+        cor_ws.to_csv('data/results/raw/'+str(self.year_test)+'_'+time_res+'_'+str(self.num_clu)+'_cor_ws.csv', index = None)
+        cor_cf.to_csv('data/results/raw/'+str(self.year_test)+'_'+time_res+'_'+str(self.num_clu)+'_cor_cf.csv', index = None)
+        
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print("Results completed and saved. Elapsed time: {:.2f} seconds".format(elapsed_time))
+        print(" ")
+        
         self.time_res = time_res
         
         return self
--- vwf/simulation.py
+++ vwf/simulation.py
@@ -5,14 +5,13 @@ from scipy import interpolate
 
 from vwf.extras import add_times
 
-def extrapolate_wind_speed(ds, turb_info):
+def extrapolate_wind_speed(reanal_data, turb_info):
  
-    # calculating wind speed from era5 variables
-    w = ds.wnd100m * (
-        np.log(ds.height/ ds.roughness) / np.log(100 / ds.roughness))
-
-    w = w.where(w > 0 , 0)
-    w = w.where(w < 40 , 40)
+    # calculating wind speed from reanalysis dataset variables
+    ws = reanal_data.wnd100m * (
+        np.log(reanal_data.height/ reanal_data.roughness) / np.log(100 / reanal_data.roughness))
+    ws = ws.where(ws > 0 , 0)
+    ws = ws.where(ws < 40 , 40)
     
     # creating coordinates to spatially interpolate to
     lat =  xr.DataArray(turb_info['lat'], dims='turbine', coords={'turbine':turb_info['ID']})
@@ -20,89 +19,80 @@ def extrapolate_wind_speed(ds, turb_info):
     height =  xr.DataArray(turb_info['height'], dims='turbine', coords={'turbine':turb_info['ID']})
 
     # spatial interpolating to turbine positions
-    wnd_spd = w.interp(
+    raw_ws = ws.interp(
             x=lon, y=lat, height=height,
             kwargs={"fill_value": None})
     
-    return wnd_spd
+    return raw_ws
 
 
-def speed_to_power(speed_frame, turb_info, powerCurveFile, train=False): 
+def speed_to_power(sim_ws, turb_info, powerCurveFile, train=False): 
     x = powerCurveFile['data$speed']
     
     if train == True:
-        turb_name = turb_info.loc[turb_info['ID'] == speed_frame.turbine.data, 'model']
+        turb_name = turb_info.loc[turb_info['ID'] == sim_ws.turbine.data, 'model']
         y = powerCurveFile[turb_name].to_numpy().flatten()
-        f2 = interpolate.Akima1DInterpolator(x, y)
-        
-        return f2(speed_frame.data)
+        f = interpolate.Akima1DInterpolator(x, y)
+        return f(sim_ws.data)
 
     else:
-        power_frame = speed_frame.copy()
-        for i in range(2, len(power_frame.columns)+1):          
-            speed_single = power_frame.iloc[:,i-1]
+        # identifying the model assigned to this turbine ID to access the power curve
+        # and covert the speed into power
+        sim_cf = sim_ws.copy()
+        for i in range(2, len(sim_cf.columns)+1):          
+            speed_single = sim_cf.iloc[:,i-1]
             turb_name = turb_info.loc[turb_info['ID'] == speed_single.name, 'model']           
             y = powerCurveFile[turb_name].to_numpy().flatten()
-            f2 = interpolate.Akima1DInterpolator(x, y)
-            power_frame.iloc[:,i-1] = f2(speed_single)
+            f = interpolate.Akima1DInterpolator(x, y)
+            sim_cf.iloc[:,i-1] = f(speed_single)
+        return sim_cf
 
-        return power_frame
 
-
-def simulate_wind(ds, turb_info, powerCurveFile, time_res='month',train=False, bias_correct=False, *args):
+def simulate_wind(reanal_data, turb_info, powerCurveFile, time_res='month',train=False, bias_correct=False, *args):
+    # grouping reanalysis data to speed up calculation
     all_heights = np.sort(turb_info['height'].unique())
-    ds = ds.assign_coords(
+    reanal_data = reanal_data.assign_coords(
         height=('height', all_heights))
-
-    wnd_spd = extrapolate_wind_speed(ds, turb_info)
+    # calculating wind speed from reanalysis data
+    raw_ws = extrapolate_wind_speed(reanal_data, turb_info)
     
     if train == True:
-        
         scalar, offset = args
-        # wnd_spd = (wnd_spd + offset) * scalar # equation 1 
-        wnd_spd = (wnd_spd * scalar) + offset # equation 2
-        wnd_spd = wnd_spd.where(wnd_spd > 0 , 0)
-        wnd_spd = wnd_spd.where(wnd_spd < 40 , 40)
-        
-        gen_power = speed_to_power(wnd_spd, turb_info, powerCurveFile, train)
-        
-        return np.mean(gen_power)
+        # raw_ws = (raw_ws + offset) * scalar # equation 1 
+        raw_ws = (raw_ws * scalar) + offset # equation 2
+        raw_ws = raw_ws.where(raw_ws > 0 , 0)
+        raw_ws = raw_ws.where(raw_ws < 40 , 40)
+        raw_cf = speed_to_power(raw_ws, turb_info, powerCurveFile, train)
+        return np.mean(raw_cf)
 
     else:
-        
-        # reformatting to include turbine ID for easier merging later
-        speed_frame = wnd_spd.to_pandas().reset_index()
-        gen_speed = speed_frame
+        unc_ws = raw_ws.to_pandas().reset_index()
 
-        speed_frame = speed_frame.melt(id_vars=["time"], 
-                        var_name="ID", 
-                        value_name="speed")
-
-        speed_frame = add_times(speed_frame)
-         
-        if bias_correct == True:
-            bias_factors = args[0]
-    
-            gen_speed = pd.merge(speed_frame, turb_info[['ID', 'cluster']], on='ID', how='left')  
+        if bias_correct == False:
+            unc_cf = speed_to_power(unc_ws, turb_info[['ID','model']], powerCurveFile)
+            return unc_ws, unc_cf
             
+        else:
+            bc_factors = args[0] # reading in bias correction factors
+            unc_ws = unc_ws.melt(id_vars=["time"], # adding in turbine ID for merging
+                var_name="ID", 
+                value_name="ws")
+            unc_ws = add_times(unc_ws)
+            
+            # matching the correct resolution bias correction factors
+            unc_ws = pd.merge(unc_ws, turb_info[['ID', 'cluster']], on='ID', how='left')  
             if time_res == 'year':
-                time_factors = bias_factors.groupby(['cluster'], as_index=False).agg({'scalar': 'mean', 'offset': 'mean'})
-                gen_speed = pd.merge(gen_speed, time_factors[['cluster','scalar', 'offset']],  how='left', on=['cluster'])
+                time_factors = bc_factors.groupby(['cluster'], as_index=False).agg({'scalar': 'mean', 'offset': 'mean'})
+                unc_ws = pd.merge(unc_ws, time_factors[['cluster','scalar', 'offset']],  how='left', on=['cluster'])
             
             else:        
-                gen_speed = pd.merge(gen_speed, bias_factors[['cluster', 'month','two_month','season']],  how='left', on=['cluster', 'month'])
-                time_factors = bias_factors.groupby(['cluster',time_res], as_index=False).agg({'scalar': 'mean', 'offset': 'mean'})
-                gen_speed = pd.merge(gen_speed, time_factors[['cluster', time_res, 'scalar', 'offset']],  how='left', on=['cluster', time_res])
+                unc_ws = pd.merge(unc_ws, bc_factors[['cluster', 'month','two_month','season']],  how='left', on=['cluster', 'month'])
+                time_factors = bc_factors.groupby(['cluster',time_res], as_index=False).agg({'scalar': 'mean', 'offset': 'mean'})
+                unc_ws = pd.merge(unc_ws, time_factors[['cluster', time_res, 'scalar', 'offset']],  how='left', on=['cluster', time_res])
     
-            # gen_speed['speed'] = (gen_speed.speed + gen_speed.offset) * gen_speed.scalar # equation 1 
-            gen_speed['speed'] = (gen_speed.speed * gen_speed.scalar) + gen_speed.offset # equation 2
-            
-            gen_speed = gen_speed.pivot(index=['time'], columns='ID', values='speed').reset_index()
-            gen_power = speed_to_power(gen_speed,turb_info[['ID','model']], powerCurveFile)
-            
-            return gen_speed, gen_power
-    
-        else:
-            gen_power = speed_to_power(gen_speed,turb_info[['ID','model']], powerCurveFile)
-            
-            return gen_speed, gen_power
+            # applying the bias correction factors to wind speed
+            # cor_ws['speed'] = (unc_ws.ws + unc_ws.offset) * gen_speed.scalar # equation 1 
+            unc_ws['ws'] = (unc_ws.ws * unc_ws.scalar) + unc_ws.offset # equation 2
+            cor_ws = unc_ws.pivot(index=['time'], columns='ID', values='ws').reset_index()
+            cor_cf = speed_to_power(cor_ws,turb_info[['ID','model']], powerCurveFile)
+            return cor_ws, cor_cf
