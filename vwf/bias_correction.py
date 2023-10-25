5b179e66f7060b06ebc2b502cc20da9dfdb11eef
Ellyess Benmoufok <benmoufok.ellyess@gmail.com>
2023-10-25 16:16:05 +0100

    fixed singlepoint and variable names
    this iteration I have managed to make sure my singlepoint input works and have cleaned up the code to try use the same variable names across files.

--- vwf/bias_correction.py
+++ vwf/bias_correction.py
@@ -10,10 +10,8 @@ from vwf.simulation import simulate_wind
 
 # Use K-means clustering, cluster the turbines by different number of clusters, and save the cluster labels.
 def cluster_labels(num_clu, sim_cf):
-    # for num_clu in [1, 2, 3, 5, 10, 15, 20, 30, 50, 100, 150, 200, 300, 400]:
     lat = sim_cf['lat']
     lon = sim_cf['lon']
-
     df = pd.DataFrame(list(zip(lat, lon)),
                     columns =['lat', 'lon'])
 
@@ -27,35 +25,9 @@ def cluster_labels(num_clu, sim_cf):
     )
     kmeans.fit(df)
     labels = kmeans.labels_
-    
     return labels
     
-def closest_cluster(clus_info, turb_info):
-    """
-    Assign turbines not found in training data to closest cluster.
-    """
-    
-    # making sure ID column dtype is same   
-    clus_info['ID'] = clus_info['ID'].astype(str)
-    turb_info['ID'] = turb_info['ID'].astype(str)
-    
-    avg = clus_info.groupby(['cluster'], as_index=False)[['lat','lon']].mean()
-    turb_info_new = pd.DataFrame.merge(clus_info[['ID','cluster']], turb_info, on='ID', how='right')
-
-    for i in range(len(turb_info_new)):
-        if np.isnan(turb_info_new.cluster[i]) == True:
-            # Find the cluster center closest to the new turbine
-            # - find smallest distance between the new turbine and cluster centers
-            indx = np.argmin(np.sqrt((avg.lat.values - turb_info_new.lat[i])**2 + (avg.lon.values - turb_info_new.lon[i])**2))
-            turb_info_new.cluster[i] = avg.cluster[indx]
-
-    turb_info_new = turb_info_new.reset_index(drop=True)
-
-    return turb_info_new
-    
-    
 def generate_training_data(unc_cf, obs_cf, turb_info, num_clu):
-
     unc_cf = unc_cf.groupby(pd.Grouper(key='time',freq='M')).mean().reset_index()
     unc_cf = unc_cf.melt(id_vars=["time"], 
                     var_name="ID", 
@@ -82,8 +54,29 @@ def generate_training_data(unc_cf, obs_cf, turb_info, num_clu):
     clus_info = df[['ID','lon','lat','cluster']].reset_index(drop=True)
     
     return train_data, clus_info
+    
+def closest_cluster(clus_info, turb_info):
+    """
+    Assign turbines not found in training data to closest cluster.
+    """
+    # making sure ID column dtype is same   
+    clus_info['ID'] = clus_info['ID'].astype(str)
+    turb_info['ID'] = turb_info['ID'].astype(str)
+    
+    avg = clus_info.groupby(['cluster'], as_index=False)[['lat','lon']].mean()
+    turb_info = pd.DataFrame.merge(clus_info[['ID','cluster']], turb_info, on='ID', how='right')
 
+    for i in range(len(turb_info)):
+        if np.isnan(turb_info.cluster[i]) == True:
+            # Find the cluster center closest to the new turbine
+            # - find smallest distance between the new turbine and cluster centers
+            indx = np.argmin(np.sqrt((avg.lat.values - turb_info.lat[i])**2 + (avg.lon.values - turb_info.lon[i])**2))
+            turb_info.cluster[i] = avg.cluster[indx]
 
+    turb_clustered = turb_info.reset_index(drop=True)
+
+    return turb_clustered
+    
 def training_bias(train_data, reanal_data, clus_info, year_star, year_end, powerCurveFile):
     """
     Traiaing the bias correction factors, for all clusters, every training 
@@ -103,24 +96,24 @@ def training_bias(train_data, reanal_data, clus_info, year_star, year_end, power
     Returns:
         Monthly bias correction factors for each cluster per training year.
     """
-    data_all = []
+    all_data = []
     star = 0
     end = 12
     for k in range(year_star,year_end+1):
     
-        # Find farms in the chosen year that have a cluster label
-        data_w_cluster = pd.DataFrame.merge(clus_info[['ID','cluster']], train_data.loc[train_data['year'] == k], on='ID', how='left')
+        # find farms in the chosen year that have a cluster label
+        train_clus = pd.DataFrame.merge(clus_info[['ID','cluster']], train_data.loc[train_data['year'] == k], on='ID', how='left')
     
-        # Get data for an 'average farm' for each cluster:
-        # Average lat, lon, height, capacity, simulated and observed CF for all farm in a cluster
-        # The turbine model with the most ocurrences is recorded
-        data_gen = data_w_cluster.groupby('cluster', as_index=False)[['lat','lon','height','obs_1', 
+        # get data for an 'average turbine' for each cluster:
+        # average lat, lon, height, capacity, simulated and observed CF for all farm in a cluster
+        # he turbine model with the most ocurrences is recorded
+        gen_data = train_clus.groupby('cluster', as_index=False)[['lat','lon','height','obs_1', 
                                                             'obs_2', 'obs_3', 'obs_4', 'obs_5',
                                                             'obs_6', 'obs_7', 'obs_8', 'obs_9', 'obs_10', 'obs_11', 'obs_12', 'sim_1',
                                                             'sim_2', 'sim_3', 'sim_4', 'sim_5', 'sim_6', 'sim_7', 'sim_8', 'sim_9',
                                                             'sim_10', 'sim_11', 'sim_12', ]].mean()
-        data_gen['model'] = data_w_cluster.groupby('cluster', as_index=False)['model'].agg(lambda x: pd.Series.mode(x)[0])['model']
-        data_gen['ID'] = data_w_cluster.groupby('cluster', as_index=False)['ID'].agg(lambda x: pd.Series.mode(x)[0])['ID']
+        gen_data['model'] = train_clus.groupby('cluster', as_index=False)['model'].agg(lambda x: pd.Series.mode(x)[0])['model']
+        gen_data['ID'] = train_clus.groupby('cluster', as_index=False)['ID'].agg(lambda x: pd.Series.mode(x)[0])['ID']
         
         # Main bias correction function
         # iterate through the months, then every cluster
@@ -132,16 +125,16 @@ def training_bias(train_data, reanal_data, clus_info, year_star, year_end, power
             scalar_list = []
             offset_list = []
             
-            energyInitial = data_gen.iloc[:,j+16]
-            energyTarget = data_gen.iloc[:,j+4]
+            energyInitial = gen_data.iloc[:,j+16]
+            energyTarget = gen_data.iloc[:,j+4]
             prt = energyTarget/energyInitial
             # iterate through the farm clusters
             for i in range(len(prt)):
                 PR = prt[i]
                 # Find scalar depending on PR = energyTarget/energyInitial
-                scalar = determine_farm_scalar(PR, 2)
+                scalar = determine_farm_scalar(PR)
                 # Find offset using the iterative process
-                offset = find_farm_offset(data_gen.iloc[[i]], reanal_data.sel(time=slice(str(k)+'-'+str(j+1)+'-01', str(k)+'-'+str(j+1)+'-'+str(monthrange(k, j+1)[1]))), powerCurveFile, scalar, energyInitial[i], energyTarget[i])
+                offset = find_farm_offset(gen_data.iloc[[i]], reanal_data.sel(time=slice(str(k)+'-'+str(j+1)+'-01', str(k)+'-'+str(j+1)+'-'+str(monthrange(k, j+1)[1]))), powerCurveFile, scalar, energyInitial[i], energyTarget[i])
     
                 scalar_list.append(scalar)
                 offset_list.append(offset)
@@ -152,24 +145,24 @@ def training_bias(train_data, reanal_data, clus_info, year_star, year_end, power
             
         # Concatenate results vertically for comparison and save results
         # the result is reordered such that a month follows another vertically
-        month_p = []
+        month_l = []
         lat_l = []
         lon_l = []
         clu_l = []
         sim_l = []
         obs_l = []
         for i in range(1,13):
-            mon = [i]*len(data_gen)
-            lat = data_gen.iloc[:,1]
-            lon = data_gen.iloc[:,2]
-            clu = data_gen.iloc[:,0]
+            mon = [i]*len(gen_data)
+            lat = gen_data.iloc[:,1]
+            lon = gen_data.iloc[:,2]
+            clu = gen_data.iloc[:,0]
             # simulated CF for all months
-            sim = data_gen.iloc[:,i+15]
+            sim = gen_data.iloc[:,i+15]
             # observed CF for all months
-            obs = data_gen.iloc[:,i+4]
+            obs = gen_data.iloc[:,i+4]
             lat_l.append(lat)
             lon_l.append(lon)
-            month_p.append(mon)
+            month_l.append(mon)
             clu_l.append(clu)
             sim_l.append(sim)
             obs_l.append(obs)
@@ -177,24 +170,24 @@ def training_bias(train_data, reanal_data, clus_info, year_star, year_end, power
     
         merged1 = list(itertools.chain(*scalar_all))
         merged2 = list(itertools.chain(*offset_all))
-        merged3 = list(itertools.chain(*month_p))
+        merged3 = list(itertools.chain(*month_l))
         merged4 = list(itertools.chain(*lat_l))
         merged5 = list(itertools.chain(*lon_l))
         merged6 = list(itertools.chain(*clu_l))
         merged7 = list(itertools.chain(*sim_l))
         merged8 = list(itertools.chain(*obs_l))
         df = pd.DataFrame(list(zip(merged1, merged2, merged3, merged4, merged5, merged6, merged7, merged8)),
-                    columns =['scalar', 'offset','month', 'lat', 'lon','cluster','sim','obs'])
-        df['prt'] = df['obs']/df['sim']
+                    columns =['scalar', 'offset','month', 'lat', 'lon','cluster','sim_cf','obs_cf'])
+        df['prt'] = df['obs_cf']/df['sim_cf']
     
         # print(df.groupby(['month'], as_index=False)[['scalar','offset']].mean())
     
         df['year'] = k
-        data_all.append(df)
+        all_data.append(df)
         star += 12
         end += 12
     
-    results_df = pd.concat(data_all)
+    results_df = pd.concat(all_data)
 
     return results_df.reset_index(drop=True)
 
@@ -243,16 +236,12 @@ def format_bcfactors(df):
             two.append('06')
     df['two_month'] = two
 
-    bias_factors = df.groupby(['cluster', 'month'], as_index=False).agg({'scalar': 'mean', 'offset': 'mean', 'season': 'first', 'two_month' : 'first'})
-    
-    year_start = df.year.min()
-    year_end = df.year.max()
-    num_clu = df.cluster.max()+1
+    bc_factors = df.groupby(['cluster', 'month'], as_index=False).agg({'scalar': 'mean', 'offset': 'mean', 'season': 'first', 'two_month' : 'first'})
 
-    return bias_factors
+    return bc_factors
 
 
-def find_farm_offset(gendata, azfile, powerCurveFile, myScalar, energyInitial, energyTarget):
+def find_farm_offset(gen_data, reanal_data, powerCurveFile, myScalar, energyInitial, energyTarget):
     """ 
     Iterative process to find the fixed offset beta in 
     bias correction formula w_corr = alpha*w + beta such that 
@@ -282,43 +271,46 @@ def find_farm_offset(gendata, azfile, powerCurveFile, myScalar, energyInitial, e
         # If we are still far from energytarget, increase stepsize
         myOffset += stepSize
         
-        # Calculate the simulated CF using the new offset
-        mylf = simulate_wind(azfile, gendata, powerCurveFile, 'month', True,False,myScalar,myOffset)
-        # print(mylf)
+        # calculate the mean simulated CF using the new offset
+        mean_cf = simulate_wind(
+            reanal_data,
+            gen_data,
+            powerCurveFile,
+            'month',
+            True,
+            False,
+            myScalar,
+            myOffset
+        )
 
-        # If we have overshot our target, then repeat, searching the other direction
+        # if we have overshot our target, then repeat, searching the other direction
         # ((guess < target & sign(step) < 0) | (guess > target & sign(step) > 0))
-        if mylf != 0:
-            energyGuess = np.mean(mylf)
+        if mean_cf != 0:
+            energyGuess = np.mean(mean_cf)
             if np.sign(energyGuess - energyTarget) == np.sign(stepSize):
                 stepSize = -stepSize / 2
             # If we have reached unreasonable places, stop
             if myOffset < -20 or myOffset > 20:
                 break
-        elif mylf == 0:
+        elif mean_cf == 0:
             myOffset = 0
             break
     
     return myOffset
 
-def determine_farm_scalar(PR, match_method=2):
+def determine_farm_scalar(PR):
     """ 
     Calculate scalar based on the error factor PR = CF_obs/CF_sim
+    using match method 2 from the original RN paper
     
     Inputs:
         PR: A ratio derived from PR = CF_obs/CF_sim
-        match_method: 1 or 2, to signify which method to use (see below)
-
     Returns:
         Scalar alpha used in bias correction: w_corr = alpha*w + beta 
     """
-    # This was used as an alternative method in RN
-    if match_method == 1:
-        scalar = 0.85
+
+    scalar_alpha = 0.6
+    scalar_beta = 0.2
+    scalar = (scalar_alpha * PR) + scalar_beta
     
-    # This is the method used in my study
-    if match_method == 2:
-        scalar_alpha = 0.6
-        scalar_beta = 0.2
-        scalar = (scalar_alpha * PR) + scalar_beta
     return scalar
--- vwf/preprocessing.py
+++ vwf/preprocessing.py
@@ -2,6 +2,101 @@ import xarray as xr
 import numpy as np
 import pandas as pd
 
+import time
+import utm
+from calendar import monthrange
+
+
+def prep_obs(country, year_star, year_end):
+    
+    if country == "DK":
+        """
+        For Denmark's data there had to be a lot of manual manipulation of the excel file. 
+        I had to manually match the turbines that exist in the power curves file, with Denmarks naming convention then match it to the ID's. 
+        anlaeg.xlsx is the raw file and match_turb_dk.xlsx is where the matching is done.
+        After this we are required to fill in missing turbine matches and also convert the coordinate system.
+        We also produce the observational data which is again manually seperated into yearly sheets from a megasheet for the years we desire.
+        As the observational data is power output we converted that to capacity factor with the matched turbines.
+        the ID's here are the gsrn ID
+        """
+        ##############################
+        # producing turb_info
+        ##############################
+        # reading in the messy denmark turbine info that we have matched
+        df = pd.read_excel('data/wind_data/DK/match_turb_dk.xlsx')
+        columns = ['Turbine identifier (GSRN)','Capacity (kW)','X (east) coordinate\nUTM 32 Euref89','Y (north) coordinate\nUTM 32 Euref89','Hub height (m)', 'Date of original connection to grid', 'turb_match']
+        df = df[columns]
+        rename_col = ['ID','capacity','x_east_32','y_north_32','height', 'date', 'model']
+        df.columns = rename_col
+        df = df.dropna()
+
+        # matching modelless turbines with closest model via capacity
+        metadata = pd.read_csv('data/turbine_info/models.csv')
+        metadata = metadata.sort_values('capacity')
+
+        df['model'][df['model'] == 0] = np.nan
+        df['capacity'] = df['capacity'].astype(int)
+        df = df.sort_values('capacity').reset_index(drop=True)
+        df.loc[df['model'].isna(), 'model'] = pd.merge_asof(df, metadata, left_on=["capacity"], right_on=["capacity"], direction="nearest")['model_y']
+
+        # convert coordinate system
+        def rule(row):
+            lat, lon = utm.to_latlon(row["x_east_32"], row["y_north_32"], 32, 'W')
+            return pd.Series({"lat": lat, "lon": lon})
+
+        df = df.merge(df.apply(rule, axis=1), left_index= True, right_index= True)
+        df = df[['ID','capacity','lat','lon','height', 'date', 'model']]
+        df['ID'] = df['ID'].astype(str)
+        turb_info = df.drop(df[df['height'] < 1].index).reset_index(drop=True)
+
+
+        ##############################
+        # producing obs_cf
+        ##############################
+
+        # Load observation data and slice the observed CF for chosen years
+        appended_data = []
+        for i in range(year_star, year_end+2): # change this back to +1 when i dont need 2020 obs
+            data = pd.read_excel('data/wind_data/DK/observation/Denmark_'+str(i)+'.xlsx')
+            data = data.iloc[3:,np.r_[0:1, 3:15]] # the slicing done here is file dependent please consider this when other files are used
+            data.columns = ['ID','1','2','3','4','5','6','7','8','9','10','11','12']
+            data['ID'] = data['ID'].astype(str)
+            data = data.reset_index(drop=True)
+            data['year'] = i
+
+            appended_data.append(data[:-1])
+
+        obs_gen = pd.concat(appended_data).reset_index(drop=True)
+        obs_gen.columns = [f'obs_{i}' if i not in ['ID', 'year'] else f'{i}' for i in obs_gen.columns]
+
+        # converting obs_gen into obs_cf by turning power into capacity factor
+        df = pd.merge(obs_gen, turb_info[['ID', 'capacity']],  how='left', on=['ID'])
+        df = df.dropna().reset_index(drop=True)
+
+        def daysDuringMonth(yy, m):
+            result = []    
+            [result.append(monthrange(y, m)[1]) for y in yy]        
+            return result
+
+        for i in range(1,13):
+            df['obs_'+str(i)] = df['obs_'+str(i)]/(((daysDuringMonth(df.year, i))*df['capacity'])*24)
+
+        df['cf_mean'] = df.iloc[:,1:13].mean(axis=1)
+        df = df.drop(df[df['cf_mean'] <= 0.01].index)
+        obs_cf = df.drop(['capacity','cf_mean'], axis=1).reset_index(drop=True)
+
+
+        turb_info.to_csv('data/wind_data/DK/turb_info_train.csv', index = None)
+        obs_cf.to_csv('data/wind_data/DK/obs_cf_train.csv', index = None)
+
+
+        # creating a turb_info_test for 2020
+        turb_info_test = turb_info.loc[turb_info['ID'].isin(obs_cf[obs_cf['year'] == 2020]['ID'])].reset_index(drop=True)
+        turb_info_test.to_csv('data/wind_data/DK/turb_info.csv', index = None)
+        
+        return obs_cf, turb_info
+
+
 def prep_era5(year_star, year_end):
     """
     Reading a saved ERA5 file with 100m wind speeds and fsr.
