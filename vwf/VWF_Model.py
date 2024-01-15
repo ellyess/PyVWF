bf31679efb82bc3e67d5e9ea8b78c5951365d3e8
Ellyess Benmoufok <benmoufok.ellyess@gmail.com>
2024-01-15 16:54:59 +0000

    Big changes in matching turbines and run structure
    “The power density determines the shape of the power curve (i.e. at what speed you reach 25, 50, 75, 100% capacity factor), so is more important than peak power output.”
    
    Turbines are not averaged but simulated on every individual turbine in a cluster.
    Weighted average based on capacity is implemented.

--- vwf/VWF_Model.py
+++ vwf/VWF_Model.py
@@ -32,7 +32,7 @@ class VWF():
     
     def prep(self):
         
-        powerCurveFileLoc = 'data/turbine_info/Wind Turbine Power Curves.csv'
+        powerCurveFileLoc = 'data/input/power_curves.csv'
         self.powerCurveFile = pd.read_csv(powerCurveFileLoc)
             
         return self
@@ -41,17 +41,18 @@ class VWF():
     def train(self, cluster_list, time_res_list):
         
         era5 = prep_era5(True)
+            
         obs_cf, turb_info = prep_obs(self.country, True)
-        turb_info.to_csv('data/correction_factors/simulated_turbines/'+self.country+'_train_turb_info.csv', index = None)
+        turb_info.to_csv('data/training/simulated-turbines/'+self.country+'_train_turb_info.csv', index = None)
         gen_cf = merge_gen_cf(era5, obs_cf, turb_info, self.powerCurveFile)
         
         for num_clu in cluster_list:
             clus_info = cluster_turbines(num_clu, turb_info)
-            clus_info.to_csv('data/correction_factors/simulated_turbines/'+self.country+'_clus_info_'+str(num_clu)+'.csv')
+            clus_info.to_csv('data/training/simulated-turbines/'+self.country+'_clus_info_'+str(num_clu)+'.csv', index = None)
         
             for time_res in time_res_list:
                 
-                my_file = Path('data/correction_factors/'+self.country+'_factors_'+time_res+'_'+str(num_clu)+'.csv')
+                my_file = Path('data/training/correction-factors/'+self.country+'_factors_'+time_res+'_'+str(num_clu)+'.csv')
                 if my_file.is_file():
                     print(time_res, " on ", num_clu,  " clusters is trained already.")
                     print(" ")
@@ -69,7 +70,7 @@ class VWF():
                         
                     ddf["offset"] = ddf.map_partitions(find_offset_parallel, meta=('offset', 'float'))
                     ddf.to_csv(
-                        'data/correction_factors/'+self.country+'_factors_'+time_res+'_'+str(num_clu)+'.csv', 
+                        'data/training/correction-factors/'+self.country+'_factors_'+time_res+'_'+str(num_clu)+'.csv', 
                         single_file=True, 
                         compute_kwargs={'scheduler':'processes'}
                     )
@@ -84,29 +85,30 @@ class VWF():
     
     def test(self, year_test, cluster_list, time_res_list):
         era5 = prep_era5()
+        
         obs_cf, turb_info = prep_obs(self.country, False, year_test)
-        obs_cf.to_csv('data/results/raw/'+self.country+"_"+str(year_test)+'_obs_ws.csv', index = None)
-        turb_info.to_csv('data/correction_factors/simulated_turbines/'+self.country+'_'+str(year_test)+'_turb_info.csv', index = None)
+        obs_cf.to_csv('data/results/capacity-factor/'+self.country+"_"+str(year_test)+'_obs_cf.csv', index = None)
+        turb_info.to_csv('data/training/simulated-turbines/'+self.country+'_'+str(year_test)+'_turb_info.csv', index = None)
         
         unc_ws, unc_cf = simulate_wind(era5, turb_info, self.powerCurveFile)
-        unc_ws.to_csv('data/results/raw/'+self.country+"_"+str(year_test)+'_unc_ws.csv', index = None)
-        unc_cf.to_csv('data/results/raw/'+self.country+"_"+str(year_test)+'_unc_cf.csv', index = None)
+        unc_ws.to_csv('data/results/wind-speed/'+self.country+"_"+str(year_test)+'_unc_ws.csv', index = None)
+        unc_cf.to_csv('data/results/capacity-factor/'+self.country+"_"+str(year_test)+'_unc_cf.csv', index = None)
             
         for num_clu in cluster_list:
-            clus_info = pd.read_csv('data/correction_factors/simulated_turbines/'+self.country+'_clus_info_'+str(num_clu)+'.csv')
+            clus_info = pd.read_csv('data/training/simulated-turbines/'+self.country+'_clus_info_'+str(num_clu)+'.csv')
             clus_info = closest_cluster(clus_info, turb_info)
         
             for time_res in time_res_list:
                 print("Test for ", num_clu, " clusters with time resolution: ", time_res, " is taking place.")
                 start_time = time.time()
                 
-                bias_data = pd.read_csv('data/correction_factors/'+self.country+'_factors_'+time_res+'_'+str(num_clu)+'.csv')
+                bias_data = pd.read_csv('data/training/correction-factors/'+self.country+'_factors_'+time_res+'_'+str(num_clu)+'.csv')
                 bc_factors = bias_data.groupby(['cluster', 'time_slice'], as_index=False).agg({'scalar': 'mean', 'offset': 'mean'})
                 bc_factors.columns = ['cluster',time_res,'scalar','offset']
         
                 cor_ws, cor_cf = simulate_wind(era5, clus_info, self.powerCurveFile, bc_factors, time_res)
-                cor_ws.to_csv('data/results/raw/'+self.country+"_"+str(year_test)+'_'+time_res+'_'+str(num_clu)+'_cor_ws.csv', index = None)
-                cor_cf.to_csv('data/results/raw/'+self.country+"_"+str(year_test)+'_'+time_res+'_'+str(num_clu)+'_cor_cf.csv', index = None)
+                cor_ws.to_csv('data/results/wind-speed/'+self.country+"_"+str(year_test)+'_'+time_res+'_'+str(num_clu)+'_cor_ws.csv', index = None)
+                cor_cf.to_csv('data/results/capacity-factor/'+self.country+"_"+str(year_test)+'_'+time_res+'_'+str(num_clu)+'_cor_cf.csv', index = None)
         
                 end_time = time.time()
                 elapsed_time = end_time - start_time
--- vwf/extras.py
+++ vwf/extras.py
@@ -24,66 +24,140 @@ def add_time_res(df):
     df['yearly'] = 'year'
     return df
 
+def weighted_monthly_cf(df, turb_info):
+    df = df.groupby(pd.Grouper(key='time',freq='M')).mean()
+    df = df.reset_index()
+    df = df.melt(
+            id_vars=["time"], # adding in turbine ID for merging
+            var_name="ID", 
+            value_name="cf"
+        )
+    
+    df["ID"] = df["ID"].astype(str)
+    turb_info["ID"] = turb_info["ID"].astype(str)
+    df = pd.merge(df, turb_info[['ID','capacity']], on=['ID'], how='left')
 
-def calc_metrics_era5(cluster_list, year_test, country):
-    time_res_list = ['yearly', 'season', 'bimonth', 'month'] 
+    def weighted_avg(df, values, weights):
+            return (df[values] * df[weights]).sum() / df[weights].sum()
+
+    wavg = lambda x: weighted_avg(df.loc[x.index], 'cf', 'capacity')
     
-    # importing observation for denmark 2020.
-    cf_obs = obs = pd.read_csv('data/results/raw/'+country+"_"+str(year_test)+'_obs_cf.csv', parse_dates=['time'])
-    cf_obs_month = cf_obs.groupby(pd.Grouper(key='time',freq='M')).mean().transpose().mean()
+    df = df.groupby(pd.Grouper(key='time',freq='M')).agg({"cf": wavg})
+    return df.cf
     
+def calc_metrics_era5(year_test, country, cluster_list, time_res_list=['yearly', 'season', 'bimonth', 'month']):
+    turb_info = pd.read_csv('data/training/simulated-turbines/'+country+"_"+str(year_test)+'_turb_info.csv')
+    cf_obs = pd.read_csv('data/results/capacity-factor/'+country+"_"+str(year_test)+'_obs_cf.csv', parse_dates=['time'])
+    cf_unc = pd.read_csv('data/results/capacity-factor/'+country+"_"+str(year_test)+'_unc_cf.csv', parse_dates=['time'])
+
+    cf_obs_month = weighted_monthly_cf(cf_obs, turb_info)
+    cf_unc_month = weighted_monthly_cf(cf_unc, turb_info)
+
     months = np.array(range(1,13))
+    diff = cf_unc_month - cf_obs_month
+    abs_err = abs(diff)
     
+    df_unc = pd.DataFrame({'month':months, 'abs_err':abs_err, 'diff': diff})
+    df_unc['num_clu'] = 1
+    df_unc['time_res'] = 'uncorrected'
+
+    # loading and calculating every scenario we simulated for comparison
     abs_diff_calc = []
     cluster_all = []
     time_all = []
     month_all = []
     diff_calc = []
-
-    
-    cf_uncorr = pd.read_csv('data/results/raw/'+country+"_"+str(year_test)+'_unc_cf.csv', parse_dates=['time'])
-    cf_month_uncorr = cf_uncorr.groupby(pd.Grouper(key='time',freq='M')).mean().transpose().mean().values
-    diff = cf_month_uncorr - cf_obs_month 
-    abs_err = abs(diff)
-
-    df_unc = pd.DataFrame({'month':months, 'abs_err':abs_err, 'diff': diff})
-    df_unc['num_clu'] = 1
-    df_unc['time_res'] = 'uncorrected'
-    
     for num_clu in cluster_list:
         for time_res in time_res_list:
+            cf_cor = pd.read_csv('data/results/capacity-factor/'+country+"_"+str(year_test)+'_'+time_res+'_'+str(num_clu)+'_cor_cf.csv', parse_dates=['time'])
+            cf_cor_month = weighted_monthly_cf(cf_cor, turb_info)
             
-            cf_corr = pd.read_csv('data/results/raw/'+country+"_"+str(year_test)+'_'+time_res+'_'+str(num_clu)+'_cor_cf.csv', parse_dates=['time'])
-            cf_month = cf_corr.groupby(pd.Grouper(key='time',freq='M')).mean().transpose().mean().values
-            diff = cf_month - cf_obs_month 
+            diff = cf_cor_month - cf_obs_month 
             abs_diff = abs(diff)
+            
             abs_diff_calc.append(abs_diff)
             diff_calc.append(diff)
-            
             cluster_all.append([num_clu]*12)
             time_all.append([time_res]*12)
             month_all.append(months)
                 
 
-
-    df_corr = pd.DataFrame(list(zip(np.ravel(cluster_all), np.ravel(time_all), np.ravel(month_all), np.ravel(abs_diff_calc), np.ravel(diff_calc))), 
+    df_cor = pd.DataFrame(list(zip(np.ravel(cluster_all), np.ravel(time_all), np.ravel(month_all), np.ravel(abs_diff_calc), np.ravel(diff_calc))), 
                  columns =['num_clu', 'time_res', 'month', 'abs_err', 'diff'])
-
-    columns = ['num_clu', 'time_res', 'month', 'abs_err', 'diff']
-    df_corr = df_corr[columns]
     
-    df_metrics = pd.concat([df_corr,df_unc]).reset_index(drop=True)
+    df_metrics = pd.concat([df_cor,df_unc]).reset_index(drop=True)
     
-    
-    # converting it to rmse and mae per trained model
+    # converting it to rmse and mae
     df_metrics['se'] = df_metrics['abs_err']**2
-    all_metrics = df_metrics
     rmse = np.sqrt(df_metrics.groupby(['num_clu', 'time_res'])['se'].mean()).reset_index()
     clus_metrics = df_metrics.groupby(['num_clu', 'time_res'])['abs_err'].mean().reset_index()
     clus_metrics['RMSE'] = rmse['se']
     clus_metrics.columns = ['num_clu', 'time_res', 'MAE', 'RMSE']
+
+    df_metrics['se'] = np.sqrt(df_metrics['se'])
+    df_metrics.columns = ['num_clu','time_res','month', 'AE', 'diff', 'RMSE']
     
-    clus_metrics.to_csv('data/results/'+country+"_"+str(year_test)+'_metrics.csv', index = None)
+    return clus_metrics, df_metrics
     
-    return clus_metrics, all_metrics
+# def calc_metrics_era5(cluster_list, year_test, country):
+#     time_res_list = ['yearly', 'season', 'bimonth', 'month'] 
+    
+#     # importing observation for denmark 2020.
+#     cf_obs = obs = pd.read_csv('data/results/capacity-factor/'+country+"_"+str(year_test)+'_obs_cf.csv', parse_dates=['time'])
+#     cf_obs_month = cf_obs.groupby(pd.Grouper(key='time',freq='M')).mean().transpose().mean()
+    
+#     months = np.array(range(1,13))
+    
+#     abs_diff_calc = []
+#     cluster_all = []
+#     time_all = []
+#     month_all = []
+#     diff_calc = []
+
+    
+#     cf_uncorr = pd.read_csv('data/results/capacity-factor/'+country+"_"+str(year_test)+'_unc_cf.csv', parse_dates=['time'])
+#     cf_month_uncorr = cf_uncorr.groupby(pd.Grouper(key='time',freq='M')).mean().transpose().mean().values
+#     diff = cf_month_uncorr - cf_obs_month 
+#     abs_err = abs(diff)
+
+#     df_unc = pd.DataFrame({'month':months, 'abs_err':abs_err, 'diff': diff})
+#     df_unc['num_clu'] = 1
+#     df_unc['time_res'] = 'uncorrected'
+    
+#     for num_clu in cluster_list:
+#         for time_res in time_res_list:
+            
+#             cf_corr = pd.read_csv('data/results/capacity-factor/'+country+"_"+str(year_test)+'_'+time_res+'_'+str(num_clu)+'_cor_cf.csv', parse_dates=['time'])
+#             cf_month = cf_corr.groupby(pd.Grouper(key='time',freq='M')).mean().transpose().mean().values
+#             diff = cf_month - cf_obs_month 
+#             abs_diff = abs(diff)
+#             abs_diff_calc.append(abs_diff)
+#             diff_calc.append(diff)
+            
+#             cluster_all.append([num_clu]*12)
+#             time_all.append([time_res]*12)
+#             month_all.append(months)
+                
+
+
+#     df_corr = pd.DataFrame(list(zip(np.ravel(cluster_all), np.ravel(time_all), np.ravel(month_all), np.ravel(abs_diff_calc), np.ravel(diff_calc))), 
+#                  columns =['num_clu', 'time_res', 'month', 'abs_err', 'diff'])
+
+#     columns = ['num_clu', 'time_res', 'month', 'abs_err', 'diff']
+#     df_corr = df_corr[columns]
+    
+#     df_metrics = pd.concat([df_corr,df_unc]).reset_index(drop=True)
+    
+    
+#     # converting it to rmse and mae per trained model
+#     df_metrics['se'] = df_metrics['abs_err']**2
+#     all_metrics = df_metrics
+#     rmse = np.sqrt(df_metrics.groupby(['num_clu', 'time_res'])['se'].mean()).reset_index()
+#     clus_metrics = df_metrics.groupby(['num_clu', 'time_res'])['abs_err'].mean().reset_index()
+#     clus_metrics['RMSE'] = rmse['se']
+#     clus_metrics.columns = ['num_clu', 'time_res', 'MAE', 'RMSE']
+    
+#     clus_metrics.to_csv('data/results/metrics/'+country+"_"+str(year_test)+'_metrics.csv', index = None)
+    
+#     return clus_metrics, all_metrics
     
