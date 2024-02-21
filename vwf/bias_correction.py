Cleaned up folders and fixed error calculation
Merra-2 test functionality added back in, Theres easy data modification for the observations for research purposes. Automatically creates folders for the results and training.

vwf/bias_correction.py
@@ -65,10 +65,6 @@ def train_data(time_res, gen_cf, clus_info):
     """
     Create data for training.
     """
-    # if time_res == 'yearly':
-    #     gen_data = gen_cf.groupby(['year','ID'], as_index=False)[['obs','sim']].mean()
-    #     gen_data['yearly'] = 'year'
-    # else:
     gen_data = gen_cf.groupby(['year',time_res,'ID'], as_index=False)[['obs','sim']].mean()
 
     gen_data = pd.merge(gen_data, clus_info[['ID', 'cluster', 'lon', 'lat', 'capacity', 'height', 'model']], on='ID', how='left')
@@ -98,7 +94,7 @@ def calculate_scalar(time_res, gen_data):
                             })
         
 
-    bias_data['scalar'] = bias_data['obs'] / bias_data['sim']
+    bias_data['scalar'] = (0.6 * (bias_data['obs'] / bias_data['sim'])) + 0.2
     bias_data = bias_data.reset_index()
     bias_data.columns = ['time_slice', 'cluster', 'year', 'obs', 'sim', 'scalar']
         
@@ -188,6 +184,40 @@ def find_offset(row, turb_info, reanalysis, powerCurveFile):
         # print(offset)
     return offset
 
+def format_bc_factors(run, time_res, num_clu, country):
+    bias_data = pd.read_csv(run+'/training/correction-factors/'+country+'_factors_'+time_res+'_'+str(num_clu)+'.csv')
+    bias_data = clean_bias_data(bias_data)
+    bias_data.columns = ['year',time_res,'cluster','scalar','offset']
+
+    bc_factors = bias_data.groupby(['cluster', time_res], as_index=False).agg({'scalar': 'mean', 'offset': 'mean'})
+    bc_factors.loc[bc_factors['scalar'].isna(), 'offset'] = 0
+    bc_factors.loc[bc_factors['scalar'].isna(), 'scalar'] = 1
+    return bc_factors
+
+def fill_factor_nan(df, time_res, num_clu, country):
+    df2 = pd.read_csv(run+'/training/correction-factors/'+country+'_factors_'+time_res+'_'+str(num_clu)+'.csv')
+    df2 = clean_bias_data(df2)
+    df2.columns = ['year',time_res,'cluster','scalar','offset']
+
+    fill = df.merge(
+        df2, 
+        on=['year','cluster',time_res],
+        how='left'
+    )
+
+    df.loc[df['scalar'].isna(), 'offset'] = fill['offset_y']
+    df.loc[df['scalar'].isna(), 'scalar'] = fill['scalar_y']
+    return df
+    
+
+def clean_bias_data(df):
+    df = df.drop(['obs','sim', 'Unnamed: 0'], axis=1)
+    df['scalar'] = df['scalar'].replace(0, np.nan)
+    return df
+    
+    
+    
+    
 # # working offset
 # def find_offset(row, turb_info, reanalysis, powerCurveFile):
 
@@ -264,7 +294,7 @@ def find_offset(row, turb_info, reanalysis, powerCurveFile):
     
 ######## POST PROCESSING OF BIAS CORRECTION FACTORS
 
-### USING FIXED TIME RES
+### USING yearly TIME RES
 # def format_bc_factors(time_res, num_clu, country):
 #     bias_data = pd.read_csv('data/training/correction-factors/'+country+'_factors_'+time_res+'_'+str(num_clu)+'.csv')
 #     bias_data = clean_bias_data(bias_data)
@@ -309,71 +339,6 @@ def find_offset(row, turb_info, reanalysis, powerCurveFile):
 #     bc_factors.loc[bc_factors['scalar'].isna(), 'offset'] = 0
 #     bc_factors.loc[bc_factors['scalar'].isna(), 'scalar'] = 1
 #     return bc_factors
-
-def format_bc_factors(time_res, num_clu, country):
-    bias_data = pd.read_csv('data/training/correction-factors/'+country+'_factors_'+time_res+'_'+str(num_clu)+'.csv')
-    bias_data = clean_bias_data(bias_data)
-    bias_data.columns = ['year',time_res,'cluster','scalar','offset']
-
-    bc_factors = bias_data.groupby(['cluster', time_res], as_index=False).agg({'scalar': 'mean', 'offset': 'mean'})
-    bc_factors.loc[bc_factors['scalar'].isna(), 'offset'] = 0
-    bc_factors.loc[bc_factors['scalar'].isna(), 'scalar'] = 1
-    return bc_factors
-
-def fill_factor_nan(df, time_res, num_clu, country):
-    df2 = pd.read_csv('data/training/correction-factors/'+country+'_factors_'+time_res+'_'+str(num_clu)+'.csv')
-    df2 = clean_bias_data(df2)
-    df2.columns = ['year',time_res,'cluster','scalar','offset']
-
-    fill = df.merge(
-        df2, 
-        on=['year','cluster',time_res],
-        how='left'
-    )
-
-    df.loc[df['scalar'].isna(), 'offset'] = fill['offset_y']
-    df.loc[df['scalar'].isna(), 'scalar'] = fill['scalar_y']
-    return df
-    
-def bias_time_res(df, time_res):
-    """
-    Add columns to identify time resolutions.
-    """
-    if time_res == 'month':
-        df.loc[df['month'] == 1, ['bimonth','season']] = ['1/6', 'winter']
-        df.loc[df['month'] == 2, ['bimonth','season']] = ['1/6', 'winter']
-        df.loc[df['month'] == 3, ['bimonth','season']] = ['2/6', 'spring']
-        df.loc[df['month'] == 4, ['bimonth','season']] = ['2/6', 'spring']
-        df.loc[df['month'] == 5, ['bimonth','season']] = ['3/6', 'spring']
-        df.loc[df['month'] == 6, ['bimonth','season']] = ['3/6', 'summer']
-        df.loc[df['month'] == 7, ['bimonth','season']] = ['4/6', 'summer']
-        df.loc[df['month'] == 8, ['bimonth','season']] = ['4/6', 'summer']
-        df.loc[df['month'] == 9, ['bimonth','season']] = ['5/6', 'autumn']
-        df.loc[df['month'] == 10, ['bimonth','season']] = ['5/6', 'autumn']
-        df.loc[df['month'] == 11, ['bimonth','season']] = ['6/6', 'autumn']
-        df.loc[df['month'] == 12, ['bimonth','season']] = ['6/6', 'winter']
-        
-    elif time_res == 'bimonth':
-        df.loc[df['bimonth'] == '1/6', 'season'] = 'winter'
-        df.loc[df['bimonth'] == '2/6', 'season'] = 'spring'
-        df.loc[df['bimonth'] == '3/6', 'season'] = 'spring'
-        df.loc[df['bimonth'] == '4/6', 'season'] = 'summer'
-        df.loc[df['bimonth'] == '5/6', 'season'] = 'autumn'
-        df.loc[df['bimonth'] == '6/6', 'season'] = 'autumn'
-        df = pd.concat([df, df.loc[df['bimonth'] == '3/6'].assign(**{'season': 'summer'})])
-        df = pd.concat([df, df.loc[df['bimonth'] == '6/6'].assign(**{'season': 'winter'})])
-
-    df['yearly'] = '1/1'
-    return df
-
-def clean_bias_data(df):
-    df = df.drop(['obs','sim', 'Unnamed: 0'], axis=1)
-    df['scalar'] = df['scalar'].replace(0, np.nan)
-    return df
-    
-    
-    
-    
     
 # # with modification for iains method
 # def calculate_scalar(time_res, gen_data, scalar_alpha = 0.6, scalar_beta = 0.2):
@@ -412,3 +377,35 @@ def clean_bias_data(df):
 #     bias_data.columns = ['time_slice', 'cluster', 'year', 'obs', 'sim', 'scalar']
         
 #     return bias_data[['year', 'time_slice', 'cluster', 'obs', 'sim', 'scalar']]
+
+
+# def bias_time_res(df, time_res):
+#     """
+#     Add columns to identify time resolutions.
+#     """
+#     if time_res == 'month':
+#         df.loc[df['month'] == 1, ['bimonth','season']] = ['1/6', 'winter']
+#         df.loc[df['month'] == 2, ['bimonth','season']] = ['1/6', 'winter']
+#         df.loc[df['month'] == 3, ['bimonth','season']] = ['2/6', 'spring']
+#         df.loc[df['month'] == 4, ['bimonth','season']] = ['2/6', 'spring']
+#         df.loc[df['month'] == 5, ['bimonth','season']] = ['3/6', 'spring']
+#         df.loc[df['month'] == 6, ['bimonth','season']] = ['3/6', 'summer']
+#         df.loc[df['month'] == 7, ['bimonth','season']] = ['4/6', 'summer']
+#         df.loc[df['month'] == 8, ['bimonth','season']] = ['4/6', 'summer']
+#         df.loc[df['month'] == 9, ['bimonth','season']] = ['5/6', 'autumn']
+#         df.loc[df['month'] == 10, ['bimonth','season']] = ['5/6', 'autumn']
+#         df.loc[df['month'] == 11, ['bimonth','season']] = ['6/6', 'autumn']
+#         df.loc[df['month'] == 12, ['bimonth','season']] = ['6/6', 'winter']
+        
+#     elif time_res == 'bimonth':
+#         df.loc[df['bimonth'] == '1/6', 'season'] = 'winter'
+#         df.loc[df['bimonth'] == '2/6', 'season'] = 'spring'
+#         df.loc[df['bimonth'] == '3/6', 'season'] = 'spring'
+#         df.loc[df['bimonth'] == '4/6', 'season'] = 'summer'
+#         df.loc[df['bimonth'] == '5/6', 'season'] = 'autumn'
+#         df.loc[df['bimonth'] == '6/6', 'season'] = 'autumn'
+#         df = pd.concat([df, df.loc[df['bimonth'] == '3/6'].assign(**{'season': 'summer'})])
+#         df = pd.concat([df, df.loc[df['bimonth'] == '6/6'].assign(**{'season': 'winter'})])
+
+#     df['yearly'] = '1/1'
+#     return df

vwf/extras.py
@@ -1,102 +1,199 @@
 import numpy as np
 import pandas as pd
 
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib
 
-def weighted_monthly_cf(df, turb_info):
-    """
-    Calculates the weighted monthly capacity factor by capacity.
+bg_colour = '#f0f0f0'
+custom_params = {'xtick.bottom': True, 'axes.edgecolor': 'black', 'axes.spines.right': False, 'axes.spines.top': False, 'axes.facecolor': bg_colour, 'mathtext.default': 'regular'}
+sns.set_theme(style='darkgrid', rc=custom_params)
+
+def calc_error(type, df_sim, df_obs, turb_info, train=False):
+    if train == True:
+        df_obs = df_obs.pivot(
+                index=['year','month'], 
+                columns='ID', 
+                values='obs').reset_index(drop=True)
+        df_obs['time'] = np.arange(str(2015)+'-01', str(2019+1)+'-01', dtype='datetime64[M]')
+        df_obs['month'] = df_obs.time.dt.month
+        df_obs_monthly = df_obs.drop(columns=['time']).groupby('month').mean().reset_index()
+        df_obs_monthly = df_obs_monthly.melt(id_vars=['month'], var_name='ID', value_name='cf')
+    else:
+        df_obs['time'] = pd.to_datetime(df_obs['time'])
+        df_obs['month'] = df_obs.time.dt.month
+        df_obs_monthly = df_obs.drop(columns=['time']).set_index('month').reset_index()
+        df_obs_monthly = df_obs_monthly.melt(id_vars=['month'], var_name='ID', value_name='cf')
     
-    Args:
-        df (pandas.DataFrame): time-series of capacity factors for turbines/farms
-        turb_info (pandas.DataFrame): turbine metadata including height and coordinates
-        
-    Returns:
-        series: weighted average of capacity factor for dataframe
-    """
-    df = df.groupby(pd.Grouper(key='time',freq='M')).mean()
-    df = df.reset_index()
-    df = df.melt(
-            id_vars=["time"], # adding in turbine ID for merging
-            var_name="ID", 
-            value_name="cf"
-        )
+    df_sim['time'] = pd.to_datetime(df_sim['time'])
+    df_sim['month'] = df_sim.time.dt.month
+    df_sim_monthly = df_sim.drop(columns=['time']).groupby('month').mean().reset_index()
+    df_sim_monthly = df_sim_monthly.melt(id_vars=['month'], var_name='ID', value_name='cf')
+
+    turb_info['ID'] = turb_info['ID'].apply(str)
+    df_obs_monthly['ID'] = df_obs_monthly['ID'].apply(str)
+    df_sim_monthly['ID'] = df_sim_monthly['ID'].apply(str)
     
-    df["ID"] = df["ID"].astype(str)
-    turb_info["ID"] = turb_info["ID"].astype(str)
-    df = pd.merge(df, turb_info[['ID','capacity']], on=['ID'], how='left')
+    merged = pd.merge(df_sim_monthly, df_obs_monthly, on=['ID', 'month'], suffixes=('_sim', '_obs'))
+    if type == 'region':
+        merged = pd.merge(merged, turb_info[['ID', 'capacity','region']], on='ID')
+    else:
+        merged = pd.merge(merged, turb_info[['ID', 'capacity']], on='ID')
+    
+    merged['diff'] = merged['cf_sim'] - merged['cf_obs']
+    merged['abdiff'] = np.abs(merged['diff'])
+    merged['sqdiff'] = merged['diff']**2
+    
+    merged = merged.dropna(subset=['cf_sim', 'cf_obs', 'capacity'])
 
     def weighted_avg(df, values, weights):
-            return (df[values] * df[weights]).sum() / df[weights].sum()
+        return (df[values] * df[weights]).sum() / df[weights].sum()
 
-    wavg = lambda x: weighted_avg(df.loc[x.index], 'cf', 'capacity')
-    
-    df = df.groupby(pd.Grouper(key='time',freq='M')).agg({"cf": wavg})
-    return df.cf
-    
-def calc_metrics_era5(year_test, country, cluster_list, time_res_list=['yearly', 'season', 'bimonth', 'month']):
-    """
-    Calculates metrics for all scenarios using ERA-5.
-    
-    Args:
-        year_test (int): year for the model to simulate
-        country (str): country code e.g. Denmark "DK"
-        cluster_list (list): list of number of clusters
-        time_res_listlist (list): list of time resolutions
-        
-    Returns:
-        clus_metrics (pandas.DataFrame): metrics calculated at per cluster
-        clus_metrics (pandas.DataFrame): metrics calculated at per turbine/farm
-    """
-    turb_info = pd.read_csv('data/training/simulated-turbines/'+country+"_"+str(year_test)+'_turb_info.csv')
-    cf_obs = pd.read_csv('data/results/capacity-factor/'+country+"_"+str(year_test)+'_obs_cf.csv', parse_dates=['time'])
-    cf_unc = pd.read_csv('data/results/capacity-factor/'+country+"_"+str(year_test)+'_unc_cf.csv', parse_dates=['time'])
+    wavg = lambda x: weighted_avg(merged.loc[x.index], 'sqdiff', 'capacity')
+    wavg2 = lambda x: weighted_avg(merged.loc[x.index], 'abdiff', 'capacity')
+    wavg3 = lambda x: weighted_avg(merged.loc[x.index], 'cf_obs', 'capacity')
 
-    cf_obs_month = weighted_monthly_cf(cf_obs, turb_info)
-    cf_unc_month = weighted_monthly_cf(cf_unc, turb_info)
+    # normalises the error i think???
+    if type == 'total':
+        error = merged.agg({"sqdiff": wavg, 'abdiff': wavg2, 'cf_obs': wavg3})
+        rmse = (np.sqrt(error['sqdiff'])/error['cf_obs'])
+        mae = (error['abdiff']/error['cf_obs'])
+    else:
+        error = merged.groupby(type).agg({"sqdiff": wavg, 'abdiff': wavg2, 'cf_obs': wavg3})
+        rmse = (np.sqrt(error['sqdiff'])/error['cf_obs']).rename('rmse')
+        mae = (error['abdiff']/error['cf_obs']).rename('mae')
 
-    months = np.array(range(1,13))
-    diff = cf_unc_month - cf_obs_month
-    abs_err = abs(diff)
-    
-    df_unc = pd.DataFrame({'month':months, 'abs_err':abs_err, 'diff': diff})
-    df_unc['num_clu'] = 1
-    df_unc['time_res'] = 'uncorrected'
+    # no normalisation
+    # if type == 'total':
+    #     error = merged.agg({"sqdiff": wavg, 'abdiff': wavg2, 'cf_obs': wavg3})
+    #     rmse = np.sqrt(error['sqdiff'])
+    #     mae = error['abdiff']
+    # else:
+    #     error = merged.groupby(type).agg({"sqdiff": wavg, 'abdiff': wavg2, 'cf_obs': wavg3})
+    #     rmse = np.sqrt(error['sqdiff']).rename('rmse')
+    #     mae = error['abdiff'].rename('mae')
 
-    # loading and calculating every scenario we simulated for comparison
-    abs_diff_calc = []
+    return rmse, mae
+
+def overall_error(run, country, turb_info, cluster_list, time_res_list, train, *args):
+    rmse_all = []
+    mae_all = []
     cluster_all = []
     time_all = []
-    month_all = []
-    diff_calc = []
+    
+    if train == True:
+        obs_cf = pd.read_csv(run+'/results/capacity-factor/'+country+'_train_obs_cf.csv')
+        unc_cf = pd.read_csv(run+'/results/capacity-factor/'+country+'_train_unc_cf.csv', parse_dates=['time'])
+    else:
+        year_test = args[0]
+        obs_cf = pd.read_csv(run+'/results/capacity-factor/'+country+"_"+str(year_test)+'_obs_cf.csv', parse_dates=['time'])
+        unc_cf = pd.read_csv(run+'/results/capacity-factor/'+country+"_"+str(year_test)+'_unc_cf.csv', parse_dates=['time'])
+    
+    rmse, mae = calc_error('total', unc_cf, obs_cf, turb_info, train)
+    
+    rmse_all.append(rmse)
+    mae_all.append(mae)
+    cluster_all.append(1)
+    time_all.append('uncorrected')
+    
     for num_clu in cluster_list:
         for time_res in time_res_list:
-            cf_cor = pd.read_csv('data/results/capacity-factor/'+country+"_"+str(year_test)+'_'+time_res+'_'+str(num_clu)+'_cor_cf.csv', parse_dates=['time'])
-            cf_cor_month = weighted_monthly_cf(cf_cor, turb_info)
-            
-            diff = cf_cor_month - cf_obs_month 
-            abs_diff = abs(diff)
-            
-            abs_diff_calc.append(abs_diff)
-            diff_calc.append(diff)
-            cluster_all.append([num_clu]*12)
-            time_all.append([time_res]*12)
-            month_all.append(months)
+            if train == True:
+                cor_cf = pd.read_csv(run+'/results/capacity-factor/'+country+'_train_'+time_res+'_'+str(num_clu)+'_cor_cf.csv', parse_dates=['time'])
+            else:
+                cor_cf = pd.read_csv(run+'/results/capacity-factor/'+country+"_"+str(year_test)+'_'+time_res+'_'+str(num_clu)+'_cor_cf.csv', parse_dates=['time'])
                 
+            rmse, mae = calc_error('total', cor_cf, obs_cf, turb_info, train)
+            
+            rmse_all.append(rmse)
+            mae_all.append(mae)
+            cluster_all.append(num_clu)
+            time_all.append(time_res)
+    
+    
+    df_metrics = pd.DataFrame(list(zip(np.ravel(cluster_all), np.ravel(time_all), np.ravel(rmse_all), np.ravel(mae_all))), 
+                 columns =['num_clu', 'time_res', 'rmse', 'mae'])
+    return df_metrics
 
-    df_cor = pd.DataFrame(list(zip(np.ravel(cluster_all), np.ravel(time_all), np.ravel(month_all), np.ravel(abs_diff_calc), np.ravel(diff_calc))), 
-                 columns =['num_clu', 'time_res', 'month', 'abs_err', 'diff'])
+def explore_error(metric_type, run, year_test, country, turb_info, cluster_list, time_res_list):
+    rmse_all = []
+    mae_all = []
+    cluster_all = []
+    time_all = []
+    type_all = []
     
-    df_metrics = pd.concat([df_cor,df_unc]).reset_index(drop=True)
-    
-    # converting it to rmse and mae
-    df_metrics['se'] = df_metrics['abs_err']**2
-    rmse = np.sqrt(df_metrics.groupby(['num_clu', 'time_res'])['se'].mean()).reset_index()
-    clus_metrics = df_metrics.groupby(['num_clu', 'time_res'])['abs_err'].mean().reset_index()
-    clus_metrics['RMSE'] = rmse['se']
-    clus_metrics.columns = ['num_clu', 'time_res', 'MAE', 'RMSE']
+    obs_cf = pd.read_csv(run+'/results/capacity-factor/'+country+'_'+str(year_test)+'_obs_cf.csv', parse_dates=['time'])
+    unc_cf = pd.read_csv(run+'/results/capacity-factor/'+country+'_'+str(year_test)+'_unc_cf.csv', parse_dates=['time'])
 
-    df_metrics['se'] = np.sqrt(df_metrics['se'])
-    df_metrics.columns = ['num_clu','time_res','month', 'AE', 'diff', 'RMSE']
+    rmse, mae = calc_error(metric_type, unc_cf, obs_cf, turb_info)
     
-    return clus_metrics, df_metrics
+    rmse_all.append(rmse)
+    mae_all.append(mae)
+    cluster_all.append([1]*len(rmse))
+    time_all.append(['uncorrected']*len(rmse))
+    type_all.append(rmse.index)
     
+    for num_clu in cluster_list:
+        for time_res in time_res_list:
+            cor_cf = pd.read_csv(run+'/results/capacity-factor/'+country+"_"+str(year_test)+'_'+time_res+'_'+str(num_clu)+'_cor_cf.csv', parse_dates=['time'])
+            rmse, mae = calc_error(metric_type, cor_cf, obs_cf, turb_info)
+            
+            rmse_all.append(rmse)
+            mae_all.append(mae)
+            cluster_all.append([num_clu]*len(rmse))
+            time_all.append([time_res]*len(rmse))
+            type_all.append(rmse.index)
+    
+    
+    df_metrics = pd.DataFrame(list(zip(np.ravel(type_all), np.ravel(cluster_all), np.ravel(time_all), np.ravel(rmse_all), np.ravel(mae_all))), 
+                 columns =[metric_type,'num_clu', 'time_res', 'rmse', 'mae'])
+
+    return df_metrics
+    
+    
+def plot_overall_error(run, country, df_metrics, name):
+    fig, axes = plt.subplots(1, 2, figsize=(12, 3))
+    
+    sns.lineplot(
+        x='num_clu',
+        y='rmse',
+        hue ="time_res",
+        style="time_res",
+        hue_order = ['month', 'bimonth', 'season', 'yearly'],
+        style_order= ['month', 'bimonth', 'season', 'yearly'],
+        data = df_metrics[(df_metrics['time_res'] != 'uncorrected')],
+        ax = axes[0],
+        legend = False
+    )
+    axes[0].set_xscale('log')
+    axes[0].set_ylabel('NRMSE')
+    axes[0].set_xlabel('$n_{clu}$')
+    axes[0].set_xticks([1, 10, 100,1000])
+    axes[0].get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+    
+    sns.lineplot(
+        x='num_clu',
+        y='mae',
+        hue ="time_res",
+        style= "time_res",
+        hue_order = ['month', 'bimonth', 'season', 'yearly'],
+        style_order= ['month', 'bimonth', 'season', 'yearly'],
+        data = df_metrics[(df_metrics['time_res'] != 'uncorrected')],
+        ax = axes[1],
+        legend = True
+    )
+    
+    axes[1].set_xscale('log')
+    axes[1].set_ylabel('NMAE')
+    axes[1].set_xlabel('$n_{clu}$')
+    axes[1].set_xticks([1, 10, 100,1000])
+    axes[1].get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+    
+    # fixing the legend labels and sharing it over whole plot.
+    axes[1].get_legend().remove()
+    handles, labels = plt.gca().get_legend_handles_labels()
+    labels = ['Monthly', 'Bimonthly',  'Seasonal', 'yearly'] # renaming labels
+    plt.legend(handles, labels, ncol=1, loc='right', bbox_to_anchor=(1.3, 0.5), title='$t_{freq}$')
+    plt.tight_layout()
+    plt.savefig(run+'/plots/'+country+'_'+name+'_error.png', bbox_inches='tight')
+    return plt.tight_layout()