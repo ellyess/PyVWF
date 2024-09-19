2497081baa65b31eb39ae0c512ef4a1841cad275
Ellyess Benmoufok <benmoufok.ellyess@gmail.com>
2024-09-19 13:27:04 +0100

    separating wind speed and cf functions

--- vwf/wind.py
+++ vwf/wind.py
@@ -53,39 +53,70 @@ def interpolate_wind(reanalysis, turb_info):
         'capacity':('turbine', turb_info['capacity']),
     })
     return sim_ws
+
+
+def simulate_grid_speed(reanalysis, turb_info):   
+    # heights are assigned to allow speed to be calculated for every height at every gridpoint
+    reanalysis = reanalysis.assign_coords(
+        height=('height', turb_info['height'].unique()))
     
-def simulate_wind(reanalysis, turb_info, powerCurveFile, *args):
-    """
-    Simulate wind speed and capacity factor, optionally can be corrected.
+    # calculating wind speed from reanalysis dataset variables
+    ws = reanalysis.wnd100m * (np.log(reanalysis.height/ reanalysis.roughness) / np.log(100 / reanalysis.roughness))
+    
+    return ws
 
-    Args:
-        reanalysis (xarray.Dataset): wind parameters on a grid.
-        turb_info (pandas.DataFrame): turbine metadata including height and coordinates.
-        powerCurveFile (pandas.DataFrame): capacity factor at increasing wind speeds for different models.
-        bc_factors (pandas.DataFrame, optional): correction factor for every cluster and time period in time resolution.
-        time_res (str, optional): time resolution to be set.
 
-    Returns:
-        sim_ws (pandas.DataFrame): time-series of simulated wind speeds at every turbine in turb_info.
-        sim_cf (pandas.DataFrame): time-series of simulated capacity factors of every turbine in turb_info.
-    """
-    sim_ws = interpolate_wind(reanalysis, turb_info)
+def simulate_speed(reanalysis, turb_info, *args):
+    ws = simulate_speed_grid(reanalysis, turb_info)
+    
+    # creating coordinates to spatially interpolate to
+    lat =  xr.DataArray(turb_info['lat'], dims='turbine', coords={'turbine':turb_info['ID']})
+    lon =  xr.DataArray(turb_info['lon'], dims='turbine', coords={'turbine':turb_info['ID']})
+    height =  xr.DataArray(turb_info['height'], dims='turbine', coords={'turbine':turb_info['ID']})
 
+    # spatial interpolation to turbine positions
+    sim_ws = ws.interp(
+        lon=lon, lat=lat, height=height,
+        kwargs={"fill_value": None}
+        )
+    
+    # assign models and capacity for future functions
+    sim_ws = sim_ws.assign_coords({
+        'model':('turbine', turb_info['model']),
+        'capacity':('turbine', turb_info['capacity']),
+    })
+    
+    # correct wind speed
     if len(args) >= 1: 
         bc_factors = args[0]
         time_res = args[1]
-        sim_ws = correct_wind_speed(sim_ws, time_res, bc_factors, turb_info)
+        sim_ws = correct_speed(sim_ws, time_res, bc_factors, turb_info)
+        
+    if save == True:
+        sim_ws.to_pandas().reset_index()
+    
+    return sim_ws
 
+
+
+def simulate_cf(reanalysis, turb_info, power_curves, *args):
+    sim_ws = simulate_speed(reanalysis, turb_info, *args)
+    
     def speed_to_power(data):
-        x = powerCurveFile['data$speed']          
-        y = powerCurveFile[data.model[0].data]
+        x = power_curves['data$speed']          
+        y = power_curves[data.model[0].data]
         f = interpolate.Akima1DInterpolator(x, y)
         return f(data)
+    
     sim_cf = sim_ws.groupby('model').map(speed_to_power)
     
-    return sim_ws.to_pandas().reset_index(), sim_cf.to_pandas().reset_index()
+    if save == True:
+        sim_cf.to_pandas().reset_index()
+        
+    return sim_cf
 
-def correct_wind_speed(ds, time_res, bc_factors, turb_info):
+
+def correct_speed(ds, time_res, bc_factors, turb_info):
     """
     Correct simulated windspeeds with assigned bias correction factors
 
@@ -110,7 +141,7 @@ def correct_wind_speed(ds, time_res, bc_factors, turb_info):
     ds = ds.assign_coords({'model':('turbine', turb_info['model'])})
     return ds.cor_ws   
 
-def train_simulate_wind(reanalysis, turb_info, powerCurveFile, scalar=1, offset=0):
+def train_simulate_cf(reanalysis, turb_info, power_curves, scalar=1, offset=0):
     """
     Simulate average capacity factor of desired resolution for training.
 
@@ -124,14 +155,15 @@ def train_simulate_wind(reanalysis, turb_info, powerCurveFile, scalar=1, offset=
         Returns:
             float: weighted average of simulated CF
     """ 
-    unc_ws = interpolate_wind(reanalysis, turb_info)
+    unc_ws = simulate_speed(reanalysis, turb_info)
     cor_ws = (unc_ws * scalar) + offset
     
     def speed_to_power(data):
-        x = powerCurveFile['data$speed']
-        y = powerCurveFile[data.model[0].data]
+        x = power_curves['data$speed']
+        y = power_curves[data.model[0].data]
         f = interpolate.Akima1DInterpolator(x, y)
         return f(data)
+    
     cor_cf = cor_ws.groupby('model').map(speed_to_power)
     avg_cf = cor_cf.weighted(cor_cf['capacity']).mean()
     return avg_cf.data
